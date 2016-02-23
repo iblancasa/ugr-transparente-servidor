924bccf059e72ee22e9e4b3a4b3b095fc5a78dfe
demiurgosoft <demiurgosoft@hotmail.com>
2016-02-23 12:17:14 +0100

    esqueleto de dbHandler #93

---
+++ app/dbhandler.js
@@ -0,0 +1,50 @@
+/*
+UGR Transparente. Sitio Web de la Universidad de Granada de acceso a Datos Abiertos.
+Copyright (C) 2016 Andrés Ortiz Corrales
+
+This file is part of UGR Transparente.
+
+UGR Transparente is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UGR Transparente is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+//DbHandler para MongoDB
+var mongo=require('mongodb').MongoClient;
+var config=require('../config/database');
+
+var db=null;
+
+module.exports={
+    connect: function(done){
+        mongo.connect(config.url+config.dbName,function(err,res){
+                if(!err) db=res;
+                done(err,res);       
+        });
+    },
+    close: function(){
+        if(db!==null){
+            db.close();
+            db=null;
+        }
+    },
+    insertDocument: function(done){
+        
+        
+        
+    }
+    
+    
+    
+    
+};
